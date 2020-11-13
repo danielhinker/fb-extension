0523264937718456e0c558d1eb84287e54be2daa
danielhinker <danhinker@gmail.com>
2020-11-13 08:00:28 -0500

    Add ability to select answers from the first instance

--- content.js
+++ content.js
@@ -6,9 +6,15 @@ chrome.runtime.onMessage.addListener(function (request) {
     console.log('a')
     //const a = document.querySelector("#hplogo").getAttribute('alt')
     const div = document.querySelector(".dati1w0a.qt6c0cv9.hv4rvrfc.cxgpxx05")
+    const qCount = div.firstChild.childElementCount
+    const answerArray = []
+    for (let i = 0; i < qCount; i++) {
+    let answer = div.firstChild.childNodes[i].childNodes[1].innerText
+        answerArray.push(answer)
+    }
     //console.log(a)
     //const b = document.querySelector
     //console.log(matches[0].nextElementSibling)
-    console.log(div)
+    console.log(answerArray)
 })
 
